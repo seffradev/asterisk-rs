88e83124e1acd38177c1635d296e8cdc7555ab8a
Hampus Avekvist <hampus.avekvist@hey.com>
2024-05-06 01:29:07 +0200

    feat(examples): start using ari::Client in playback example

--- examples/playback.rs
+++ examples/playback.rs
@@ -1,3 +1,4 @@
+use ari_rs::Client;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -9,6 +10,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .init();
 
     tracing::info!("Hello, world!");
+
+    let client = Client::new()
+        .url("http://localhost:8080")
+        .username("admin")
+        .password("password")
+        .connect()?
+        .build();
 
     Ok(())
 }
