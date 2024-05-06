edf45a383dc406e420bc987fcb1f103e0ecb7133
Hampus Avekvist <ha@leissner.se>
2024-05-06 18:00:29 +0200

    feat(examples): use channels instead of closures

--- examples/channels.rs
+++ examples/channels.rs
@@ -1,30 +1,49 @@
+use std::sync::Arc;
+
 use ari_rs::client::Client;
-use tracing::debug;
+use ari_rs::Result;
+use tokio::sync::mpsc;
+use tracing::{debug, error, trace};
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     tracing_subscriber::registry()
         .with(fmt::layer())
         .with(EnvFilter::from_default_env())
         .init();
 
-    let client = Client::new()
-        .url("http://localhost:8088/ari")?
-        .username("asterisk")
-        .password("asterisk")
-        .app_name("ari")
-        .connect()
-        .on_stasis_start(|client, _| Box::new(async {
-            if let Ok(channels) = client.list_channels().await {
-                for channel in channels {
-                    debug!("Channel: {:?}", channel);
-                }
-            }
-        }))
-        .build()?;
+    let (tx, mut rx) = mpsc::channel(1024);
 
-    client.run().await?;
+    let client = Arc::new(
+        Client::new()
+            .url("http://localhost:8088/ari")?
+            .username("asterisk")
+            .password("asterisk")
+            .app_name("ari")
+            .connect()
+            .handler(tx)
+            .build()?,
+    );
+
+    let join = tokio::spawn(async move {
+        trace!("Starting client");
+        if let Err(e) = client.run().await {
+            error!("Error running client: {:?}", e);
+        }
+    });
+
+    while let Some(event) = rx.recv().await {
+        trace!("Received event: {:?}", event);
+        match event {
+            ari_rs::Event::StasisStart(event) => {
+                debug!("Channel ID: {}", event.channel.id);
+            }
+            _ => debug!("Unhandled event: {:?}", event),
+        }
+    }
+
+    join.await?;
 
     Ok(())
 }
