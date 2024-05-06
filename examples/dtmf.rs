63c93ddcabfbeab3831fb192c4dfb039eb6b2695
Hampus Avekvist <ha@leissner.se>
2024-05-06 17:59:14 +0200

    feat(example): use channels instead of closures

--- examples/dtmf.rs
+++ examples/dtmf.rs
@@ -1,4 +1,4 @@
-use ari_rs::client::Client;
+use ari_rs::{client::Client, Event};
 use std::sync::{Arc, Mutex};
 use tracing::debug;
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
@@ -10,9 +10,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with(EnvFilter::from_default_env())
         .init();
 
+    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
     let dtmf_buffer = Arc::new(Mutex::new(String::new()));
-    let stasis_end_dtmf = dtmf_buffer.clone();
-    let dtmf_received_buffer = dtmf_buffer.clone();
 
     let client = Client::new()
         .url("http://localhost:8088")?
@@ -20,20 +19,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .password("asterisk")
         .app_name("ari")
         .connect()
-        .on_stasis_start(move |_, _| {
-            debug!("Resetting the DTMF buffer");
-            dtmf_buffer.lock().unwrap().clear();
-        })
-        .on_stasis_end(move |_, _| {
-            debug!("DTMF buffer: {}", stasis_end_dtmf.lock().unwrap());
-        })
-        .on_channel_dtmf_received(move |_, event| {
-            debug!("Adding DTMF digit: {}", event.digit);
-            dtmf_received_buffer.lock().unwrap().push_str(&event.digit);
-        })
+        .handler(tx)
         .build()?;
 
-    client.run().await?;
+    tokio::spawn(async move {
+        if let Err(e) = client.run().await {
+            eprintln!("Error: {}", e);
+        }
+    });
+
+    while let Some(event) = rx.recv().await {
+        match event {
+            Event::StasisStart(_) => {
+                debug!("Stasis started, resetting DTMF buffer");
+                dtmf_buffer.lock().unwrap().clear();
+            }
+            Event::StasisEnd(_) => {
+                debug!("Stasis ended, DTMF buffer: {}", dtmf_buffer.lock().unwrap());
+            }
+            Event::ChannelDtmfReceived(event) => {
+                debug!("Received DTMF: {}", event.digit);
+                dtmf_buffer.lock().unwrap().push_str(&event.digit);
+            }
+            _ => {
+                debug!("Unhandled event: {:?}", event);
+            }
+        }
+    }
 
     Ok(())
 }
