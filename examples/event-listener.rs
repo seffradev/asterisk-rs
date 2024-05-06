a5cfab658ca0700108e84c5c9658459604fe3a43
Hampus Avekvist <hampus.avekvist@hey.com>
2024-05-06 04:50:49 +0200

    feat(examples): add an event listener to find new events

---
+++ examples/event-listener.rs
@@ -0,0 +1,22 @@
+use ari_rs::client::Client;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
+    let client = Client::new()
+        .url("http://localhost:8088")?
+        .username("asterisk")
+        .password("asterisk")
+        .app_name("ari")
+        .connect()
+        .build()?;
+
+    client.run().await?;
+
+    Ok(())
+}
