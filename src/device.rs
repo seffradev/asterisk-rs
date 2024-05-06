31b83ee89fbba44e6ce6d13ae7f015b514102e8b
Hampus Avekvist <hampus.avekvist@hey.com>
2024-05-06 02:10:20 +0200

    feat(client): enable setting of device-related handlers

--- src/device.rs
+++ src/device.rs
@@ -1,6 +1,18 @@
 use chrono::DateTime;
 use serde::{Deserialize, Serialize};
 
+use crate::client::{ClientBuilder, Connected};
+
+impl ClientBuilder<Connected> {
+    pub fn on_device_state_changed<F>(mut self, f: F) -> Self
+    where
+        F: Fn(DeviceStateChanged) -> () + 'static,
+    {
+        self.data.0.on_device_state_changed = Some(Box::new(f));
+        self
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
 pub struct DeviceStateChanged {
