38455638fa844a5f8ad9c11121fb5c4a88bfa968
Hampus Avekvist <hampus.avekvist@hey.com>
2024-05-06 02:09:56 +0200

    feat(client): enable setting of channel-related handlers

--- src/channel.rs
+++ src/channel.rs
@@ -1,6 +1,74 @@
 use chrono::DateTime;
 use serde::{Deserialize, Serialize};
 
+use crate::client::{ClientBuilder, Connected};
+
+impl ClientBuilder<Connected> {
+    pub fn on_stasis_start<F>(mut self, f: F) -> Self
+    where
+        F: Fn(StasisStart) -> () + 'static,
+    {
+        self.data.0.on_stasis_start = Some(Box::new(f));
+        self
+    }
+
+    pub fn on_stasis_end<F>(mut self, f: F) -> Self
+    where
+        F: Fn(StasisEnd) -> () + 'static,
+    {
+        self.data.0.on_stasis_end = Some(Box::new(f));
+        self
+    }
+
+    pub fn on_channel_created<F>(mut self, f: F) -> Self
+    where
+        F: Fn(ChannelCreated) -> () + 'static,
+    {
+        self.data.0.on_channel_created = Some(Box::new(f));
+        self
+    }
+
+    pub fn on_channel_destroyed<F>(mut self, f: F) -> Self
+    where
+        F: Fn(ChannelDestroyed) -> () + 'static,
+    {
+        self.data.0.on_channel_destroyed = Some(Box::new(f));
+        self
+    }
+
+    pub fn on_channel_varset<F>(mut self, f: F) -> Self
+    where
+        F: Fn(ChannelVarset) -> () + 'static,
+    {
+        self.data.0.on_channel_varset = Some(Box::new(f));
+        self
+    }
+
+    pub fn on_channel_hangup_request<F>(mut self, f: F) -> Self
+    where
+        F: Fn(ChannelHangupRequest) -> () + 'static,
+    {
+        self.data.0.on_channel_hangup_request = Some(Box::new(f));
+        self
+    }
+
+    pub fn on_channel_dialplan<F>(mut self, f: F) -> Self
+    where
+        F: Fn(ChannelDialplan) -> () + 'static,
+    {
+        self.data.0.on_channel_dialplan = Some(Box::new(f));
+        self
+    }
+
+    pub fn on_channel_state_change<F>(mut self, f: F) -> Self
+    where
+        F: Fn(ChannelStateChange) -> () + 'static,
+    {
+        self.data.0.on_channel_state_change = Some(Box::new(f));
+        self
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
 pub struct StasisStart {
