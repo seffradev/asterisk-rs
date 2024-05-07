5bc884d26fa656d70fd3016262e008b3b08e206c
Hampus Avekvist <hampus.avekvist@hey.com>
2024-05-08 01:10:23 +0200

    feat(examples): add channel originate example

---
+++ examples/originate.rs
@@ -0,0 +1,39 @@
+use arirs::Result;
+use arirs::{channel::OriginateParams, client::Client};
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+const APP_NAME: &str = "ari";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
+    let client = Client::new()
+        .url("http://localhost:8088/")?
+        .username("asterisk")
+        .password("asterisk")
+        .app_name(APP_NAME)
+        .build()?;
+
+    client
+        .originate_channel(
+            "PJSIP/1000",
+            OriginateParams::Application {
+                app: APP_NAME,
+                app_args: vec![],
+            },
+            None,
+            None,
+            None,
+            None,
+            None,
+            vec!["alaw,ulaw"],
+            None,
+        )
+        .await?;
+
+    Ok(())
+}
