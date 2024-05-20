deb68891bceba06e9a5c55c6cd084fdb3360a9d1
Hampus Avekvist <ha@leissner.se>
2024-05-20 19:51:12 +0200

    refactor: split non bridge-creating functions into Bridge

--- src/bridge.rs
+++ src/bridge.rs
@@ -2,6 +2,53 @@ use crate::Result;
 use crate::{client::Client, playback::Playback, recording::Recording};
 use serde::{Deserialize, Serialize};
 
+impl Bridge {
+    pub async fn destroy(&self, _client: &Client) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub async fn add_channel(&self, _client: &Client, _channel_id: &str) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub async fn remove_channel(&self, _client: &Client, _channel_id: &str) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub async fn set_channel_as_video_source(
+        &self,
+        _client: &Client,
+        _channel_id: &str,
+        _video_source_id: &str,
+    ) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub async fn unset_video_source(&self, _client: &Client) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub async fn start_moh(&self, _client: &Client, _moh_class: &str) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub async fn stop_moh(&self, _client: &Client) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub async fn play_media(&self, _client: &Client, _playback: &Playback) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub async fn stop_media(&self, _client: &Client) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub async fn start_recording(&self, _client: &Client, _recording: &Recording) -> Result<()> {
+        unimplemented!()
+    }
+}
+
 impl Client {
     pub async fn list_bridges(&self) -> Result<Vec<Bridge>> {
         unimplemented!()
@@ -11,64 +58,15 @@ impl Client {
         unimplemented!()
     }
 
-    pub async fn create_bridge_with_id(&self, _bridge_id: &str, _bridge: &Bridge) -> Result<Bridge> {
+    pub async fn create_bridge_with_id(
+        &self,
+        _bridge_id: &str,
+        _bridge: &Bridge,
+    ) -> Result<Bridge> {
         unimplemented!()
     }
 
     pub async fn get_bridge(&self, _bridge_id: &str) -> Result<Bridge> {
-        unimplemented!()
-    }
-
-    pub async fn destroy_bridge(&self, _bridge_id: &str) -> Result<()> {
-        unimplemented!()
-    }
-
-    pub async fn add_channel_to_bridge(&self, _bridge_id: &str, _channel_id: &str) -> Result<()> {
-        unimplemented!()
-    }
-
-    pub async fn remove_channel_from_bridge(
-        &self,
-        _bridge_id: &str,
-        _channel_id: &str,
-    ) -> Result<()> {
-        unimplemented!()
-    }
-
-    pub async fn set_bridge_as_video_source(
-        &self,
-        _bridge_id: &str,
-        _channel_id: &str,
-        _video_source_id: &str,
-    ) -> Result<()> {
-        unimplemented!()
-    }
-
-    pub async fn unset_bridge_as_video_source(&self, _bridge_id: &str) -> Result<()> {
-        unimplemented!()
-    }
-
-    pub async fn start_moh_bridge(&self, _bridge_id: &str, _moh_class: &str) -> Result<()> {
-        unimplemented!()
-    }
-
-    pub async fn stop_moh_bridge(&self, _bridge_id: &str) -> Result<()> {
-        unimplemented!()
-    }
-
-    pub async fn play_bridge_media(&self, _bridge_id: &str, _playback: &Playback) -> Result<()> {
-        unimplemented!()
-    }
-
-    pub async fn stop_bridge_media(&self, _bridge_id: &str) -> Result<()> {
-        unimplemented!()
-    }
-
-    pub async fn start_bridge_recording(
-        &self,
-        _bridge_id: &str,
-        _recording: &Recording,
-    ) -> Result<()> {
         unimplemented!()
     }
 }
