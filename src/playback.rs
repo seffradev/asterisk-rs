f05ae3cdadc423d28bcc35bb4b6c179a94d26ddd
Hampus Avekvist <ha@leissner.se>
2024-06-26 11:16:51 +0200

    refactor(playback): use derive_more::Display instead of manual implementation

--- src/playback.rs
+++ src/playback.rs
@@ -1,7 +1,7 @@
 use crate::client::Client;
 use crate::Result;
 use serde::{Deserialize, Serialize};
-use std::fmt::Display;
+use derive_more::Display;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
@@ -23,24 +23,17 @@ impl Playback {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Display)]
 #[serde(rename_all = "snake_case")]
 pub enum Operation {
+    #[display(fmt = "restart")]
     Restart,
+    #[display(fmt = "pause")]
     Pause,
+    #[display(fmt = "unpause")]
     Unpause,
+    #[display(fmt = "reverse")]
     Reverse,
+    #[display(fmt = "forward")]
     Forward,
-}
-
-impl Display for Operation {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Operation::Restart => write!(f, "restart"),
-            Operation::Pause => write!(f, "pause"),
-            Operation::Unpause => write!(f, "unpause"),
-            Operation::Reverse => write!(f, "reverse"),
-            Operation::Forward => write!(f, "forward"),
-        }
-    }
 }
