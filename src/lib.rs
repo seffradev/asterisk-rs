769ddbbcee30dfaac1adbf57ed027ce3f4e23a72
Hampus Avekvist <ha@leissner.se>
2024-06-26 10:15:20 +0200

    feat: simplify errors and remove boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ use channel::{
 };
 use device::DeviceStateChanged;
 use serde::{Deserialize, Serialize};
-use std::fmt::Display;
 use thiserror::Error;
 use tokio::task::JoinError;
 
@@ -19,65 +18,24 @@ pub mod variable;
 
 pub type Result<T> = std::result::Result<T, AriError>;
 
-impl From<url::ParseError> for AriError {
-    fn from(err: url::ParseError) -> Self {
-        AriError::UrlParseError(err)
-    }
-}
-
-impl From<tungstenite::Error> for AriError {
-    fn from(err: tungstenite::Error) -> Self {
-        AriError::TungsteniteError(err)
-    }
+#[derive(Debug, Error)]
+pub enum AriError {
+    #[error("URL parsing error")]
+    UrlParseError(#[from] url::ParseError),
+    #[error("WebSocket error")]
+    TungsteniteError(#[from] tungstenite::Error),
+    #[error("HTTP Request error")]
+    ReqwestError(#[from] reqwest::Error),
+    #[error("Join Error")]
+    JoinError(#[from] JoinError),
+    #[error("Unknown error occured: {0}")]
+    Unknown(String),
 }
 
 impl From<tungstenite::error::UrlError> for AriError {
     fn from(err: tungstenite::error::UrlError) -> Self {
         AriError::TungsteniteError(err.into())
     }
-}
-
-impl From<reqwest::Error> for AriError {
-    fn from(err: reqwest::Error) -> Self {
-        AriError::ReqwestError(err)
-    }
-}
-
-impl From<JoinError> for AriError {
-    fn from(err: JoinError) -> Self {
-        AriError::JoinError(err)
-    }
-}
-
-impl From<String> for AriError {
-    fn from(err: String) -> Self {
-        AriError::Unknown(err)
-    }
-}
-
-impl Display for AriError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            AriError::UrlParseError(err) => write!(f, "UrlParseError: {}", err),
-            AriError::TungsteniteError(err) => write!(f, "TungsteniteError: {}", err),
-            AriError::ReqwestError(err) => write!(f, "ReqwestError: {}", err),
-            AriError::JoinError(err) => write!(f, "JoinError: {}", err),
-            AriError::HttpError(status, body) => {
-                write!(f, "HttpError: {} - {}", status, body)
-            }
-            AriError::Unknown(err) => write!(f, "Unknown: {}", err),
-        }
-    }
-}
-
-#[derive(Debug, Error)]
-pub enum AriError {
-    UrlParseError(url::ParseError),
-    TungsteniteError(tungstenite::Error),
-    ReqwestError(reqwest::Error),
-    JoinError(JoinError),
-    HttpError(reqwest::StatusCode, String),
-    Unknown(String),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
