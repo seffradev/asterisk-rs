44cad89298c961666332c396e9d53eebd7c3030c
gibbz00 <gabrielhansson00@gmail.com>
2024-09-14 12:55:48 +0200

    refactor!(ari): invert self to `RequestClient` in `playback`

--- arirs/src/playback.rs
+++ arirs/src/playback.rs
@@ -2,24 +2,10 @@ use serde::{Deserialize, Serialize};
 
 use crate::*;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub struct Playback {
     pub id: String,
-}
-
-impl Playback {
-    pub async fn get_playback(_client: &RequestClient, _playback_id: &str) -> Result<Playback> {
-        unimplemented!()
-    }
-
-    pub async fn control(&self, _client: &RequestClient, _operation: Operation) -> Result<()> {
-        unimplemented!()
-    }
-
-    pub async fn stop(&self, _client: &RequestClient) -> Result<()> {
-        unimplemented!()
-    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -30,4 +16,18 @@ pub enum Operation {
     Unpause,
     Reverse,
     Forward,
+}
+
+impl RequestClient {
+    pub async fn get_playback(&self, _playback_id: &str) -> Result<Playback> {
+        unimplemented!()
+    }
+
+    pub async fn control(&self, _playback_id: &str, _operation: Operation) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub async fn stop(&self, _playback_id: &str) -> Result<()> {
+        unimplemented!()
+    }
 }
