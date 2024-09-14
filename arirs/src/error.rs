c9995c7fb458c734076ecbd7af9313d3f85265c0
gibbz00 <gabrielhansson00@gmail.com>
2024-09-14 19:56:23 +0200

    chore!(ari): remove `AriError::Tungstenite`

--- arirs/src/error.rs
+++ arirs/src/error.rs
@@ -1,5 +1,4 @@
 use thiserror::Error;
-use tokio_tungstenite::tungstenite;
 
 pub type Result<T> = std::result::Result<T, AriError>;
 
@@ -7,16 +6,8 @@ pub type Result<T> = std::result::Result<T, AriError>;
 pub enum AriError {
     #[error(transparent)]
     UrlParse(#[from] url::ParseError),
-    #[error("WebSocket error")]
-    Tungstenite(#[from] tungstenite::Error),
     #[error("HTTP Request error")]
     Reqwest(#[from] reqwest::Error),
     #[error("Unknown error occurred: {0}")]
     Unknown(String),
-}
-
-impl From<tungstenite::error::UrlError> for AriError {
-    fn from(err: tungstenite::error::UrlError) -> Self {
-        AriError::Tungstenite(err.into())
-    }
 }
